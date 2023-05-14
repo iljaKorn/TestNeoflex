e3c27b45d9d3d79bf782f6f3750d1de249fc9ef1
iljaKorn <kornilov.ilja@rambler.ru>
2023-05-14 15:59:57 +0300

    Initial commit

---
+++ src/main/java/com/example/testneoflex/TestNeoflexApplication.java
@@ -0,0 +1,13 @@
+package com.example.testneoflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNeoflexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestNeoflexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testneoflex/TestNeoflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testneoflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNeoflexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
